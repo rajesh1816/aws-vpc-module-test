c0e211bf726aa8919301f43cfd7840ada4743eb0
rajesh <rajeshkaramsetty1999@gmail.com>
2025-12-01 21:40:59 +0530

    added content

---
+++ main.tf
@@ -0,0 +1,9 @@
+module "vpc" {
+  source              = "../terraform-aws-vpc-module"
+  vpc_cidr_block      = var.vpc_cidr_block
+  project             = var.project
+  environment         = var.environment
+  public_cidr_block   = var.public_cidr_block
+  private_cidr_block  = var.private_cidr_block
+  database_cidr_block = var.database_cidr_block
+}
