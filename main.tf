cf76ca7f2f6415cccf0c11a493334604cf2395d4
rajesh <rajeshkaramsetty1999@gmail.com>
2025-12-02 21:56:58 +0530

    added content

--- main.tf
+++ main.tf
@@ -6,4 +6,6 @@ module "vpc" {
   public_cidr_block   = var.public_cidr_block
   private_cidr_block  = var.private_cidr_block
   database_cidr_block = var.database_cidr_block
+  is_peering_required = true
 }
+
